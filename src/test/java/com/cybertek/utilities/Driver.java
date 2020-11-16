7b931aa7324f030b47682940a37b2c182c5e8792
Phornsri <ponsri_Molae074@yahoo.com>
2020-11-15 21:01:24 -0500

    commit1

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -3,7 +3,10 @@ package com.cybertek.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -14,29 +17,28 @@ public class Driver {
     public static WebDriver getDriver(){
 
         if(driver == null){
-
             String browser = ConfigurationReader.getProperty("browser");
             switch (browser){
-                case "chrome" :
-                    WebDriverManager.chromedriver().setup() ;
-                    driver =  new ChromeDriver() ;
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.setHeadless(true);
+                    driver = new ChromeDriver(chromeOptions);
                     break;
-
-                case "firefox" :
-                    WebDriverManager.firefoxdriver().setup() ;
-                    driver =  new FirefoxDriver() ;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
                     break;
-
-
             }
         }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return driver;
     }
-
     public static void closeDriver(){
         if(driver!=null){
-            driver.quit() ;
-            driver.close() ;
+            driver.quit();
+            driver = null;
         }
     }
 
